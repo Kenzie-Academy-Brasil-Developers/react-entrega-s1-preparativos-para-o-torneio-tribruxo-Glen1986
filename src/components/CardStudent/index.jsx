3fc4f60dc08452dcc7ad5766a5858f3322a8bfb6
glen1986 <escobar.glen@gmail.com>
2021-09-10 10:21:09 -0300

    tribMargin

--- src/components/CardStudent/index.jsx
+++ src/components/CardStudent/index.jsx
@@ -4,9 +4,10 @@ const CardStudent = ({ char: { id, name, image, house, patronus, species}}) =>{
   return(
     <div className ="card">
       <label>{name}</label>
-      <label>{house}</label>
+      <label>Casa: {house}</label>
       <img className ="picture" src={image} alt={name} />
-      <label>{patronus}</label>
+      <label> Patronus: {patronus}</label>
+      <label>Specie: {species}</label>
     </div>
   )
 
